9f5a8e236933cecc38e1ce9ac8950b61d7a2d0b1
123khushisahu <khushisahu9580@gmail.com>
2026-04-10 09:32:47 +0530

    done

---
+++ DSA/src/basics/class1.java
@@ -0,0 +1,30 @@
+package basics;
+
+public class class1 {
+
+	public static void main(String[] args) {
+System.out.println("*\n**\n***\n****\n*****\n");
+
+
+System.out.println("******");
+System.out.println("     *");
+System.out.println("    *");
+System.out.println("   *");
+System.out.println("  *");
+System.out.println(" *");
+System.out.println("*****\n");
+
+System.out.println("*****\n    *\n   *\n  *\n *\n*****\n");
+
+
+System.out.println("*    *");
+System.out.println(" *  *");
+System.out.println("   *  ");
+System.out.println(" *   *");
+System.out.println("*     *\n");
+
+
+System.out.println("*   *\n * * \n  *  \n *  * \n*    *");
+	}
+
+}
